e36865ff39967158cba39f8f98e0be4263979bb2
user2 <user2@KH_C>
2017-11-21 21:27:44 +0900

    기환아 받아

---
+++ hey_bro_project_BROS/src/View/main/Main_UI.java
@@ -0,0 +1,115 @@
+package View.main;
+
+import java.awt.Color;
+import java.awt.Image;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+import javax.swing.*;
+
+public class Main_UI {
+
+	public void main_UI(){
+		JFrame mf = new JFrame();
+
+		mf.setLayout(null);
+		mf.setBackground(Color.WHITE);
+
+		//프레임 크기
+		mf.setBounds(400, 200, 800, 500);
+		//프레임 이름
+		mf.setTitle("Hey~bro");
+		//사이즈 조절불가
+		mf.setResizable(false);
+
+
+		JPanel mainPanel = new JPanel();
+		mainPanel.setBackground(Color.WHITE);
+		mainPanel.setSize(800, 500);
+		JPanel userPanel = new JPanel();
+
+
+		userPanel.setLocation(0, 0);
+		userPanel.setBackground(Color.LIGHT_GRAY);
+		userPanel.setSize(200, 500);
+		//icon_ : 좌측버튼
+		//icon : 우측버튼
+		Image icon = new ImageIcon("img/main_1.PNG").getImage().getScaledInstance(140, 139, 0);
+		JButton plusLabel = new JButton(new ImageIcon(icon));
+
+		plusLabel.setLocation(250, 200);
+		//userPanel2.setBackground(Color.blue);
+		plusLabel.setSize(130, 130);
+
+		Image icon2 = new ImageIcon("img/main_2.PNG").getImage().getScaledInstance(140, 139, 0);
+		JButton join = new JButton(new ImageIcon(icon2));
+
+		join.setLocation(450, 200);
+		//userPanel3.setBackground(Color.green);
+		join.setSize(130, 130);
+
+		Image icon3 = new ImageIcon("img/main_3.PNG").getImage().getScaledInstance(139, 139, 0);
+		JButton in = new JButton(new ImageIcon(icon3));
+
+		in.setLocation(650, 200);
+		//userPanel4.setBackground(Color.RED);
+		in.setSize(130, 130);
+
+		Image icon_1 = new ImageIcon("img/button1.PNG").getImage().getScaledInstance(207, 52, 0);
+		JButton edit = new JButton(new ImageIcon(icon_1));
+		edit.setLocation(10, 100);
+		edit.setSize(180, 52);
+
+		Image icon_2 = new ImageIcon("img/button2.PNG").getImage().getScaledInstance(205, 48, 0);
+		JButton qa = new JButton(new ImageIcon(icon_2));
+		qa.setLocation(10, 170);
+		qa.setSize(180, 52);
+
+		Image icon_3 = new ImageIcon("img/button3.PNG").getImage().getScaledInstance(206, 50, 0);
+		JButton out = new JButton(new ImageIcon(icon_3));
+		out.setLocation(10, 350);
+		out.setSize(180, 52);
+
+		//text
+		JLabel text1 = new JLabel("님 환영합니다.");
+		text1.setLocation(110, 3);
+		text1.setSize(100, 50);
+		JLabel text2 = new JLabel("조성식");
+		text2.setLocation(65, 3);
+		text2.setSize(60, 50);
+
+		Image icon_4 = new ImageIcon("img/button4.PNG").getImage().getScaledInstance(48, 45, 0);
+		JLabel png = new JLabel(new ImageIcon(icon_4));
+		png.setLocation(0, 3);
+		png.setSize(48, 45);
+
+
+		mf.add(png);
+		mf.add(plusLabel);
+		mf.add(join);
+		mf.add(in);
+		mf.add(edit);
+		mf.add(qa);
+		mf.add(out);
+		mf.add(text1);
+		mf.add(text2);
+		mf.add(userPanel);
+		mf.add(mainPanel);
+		//mf.add();
+
+		/*//아이콘 이미지 변경
+		try {
+			mf.setIconImage(ImageIO.read(new File("img/md.png"))); 
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		 */
+
+		mf.setVisible(true);
+		mf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+	public static void main(String[] args){
+		
+	}
+}
