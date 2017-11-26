4471a4d7b2b11c0f2f4ee7d5b61b5760dacff582
user <user@DESKTOP-MFL187H>
2017-11-26 18:58:47 +0900

    fadfad

--- hey_bro_project_BROS/src/Model/dao/GroupDAO.java
+++ hey_bro_project_BROS/src/Model/dao/GroupDAO.java
@@ -5,6 +5,7 @@ import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.InvalidPropertiesFormatException;
@@ -129,6 +130,70 @@ public class GroupDAO {
 		return true; 
 		
 	}
-
+	//조성식 수정
+	public void groupUpdate(Group g){
+		Properties prop = new Properties();
+		Map<Integer, Group> groupMap = new HashMap<>();
+		ArrayList<Group> groupArrayList = new ArrayList<>();
+		
+		//
+		String rNum;
+		String rGname;
+		String rInterests;
+		String rPw;
+		String rContent;
+		String rNews;
+		String rLeader;
+		
+		try {
+			prop.loadFromXML(new FileInputStream("hey_bro_project_BROS\\src\\Model\\Data\\xml\\groups.xml"));
+		
+			for(int i = 0; i < prop.size(); i++){
+				String str;
+				
+				String[] str_arr = new String[7];
+				
+				str = prop.getProperty(String.valueOf(i).toString());
+				if(str != null){
+					str_arr = str.split(", ");
+					
+					rNum = str_arr[0];
+					rGname = str_arr[1];
+					rInterests = str_arr[2];
+					rPw = str_arr[3];
+					rContent = str_arr[4];
+					rNews = str_arr[5];
+					rLeader = str_arr[6];
+					
+					Group group = new Group(rNum, rGname, rInterests, rPw, rContent, rNews, rLeader);
+					groupMap.put(groupMap.size(), group);
+				}
+			}
+		} catch (InvalidPropertiesFormatException e) {
+			e.printStackTrace();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		Collection<Group> groupValues = groupMap.values();
+		groupArrayList.addAll(groupValues);
+		for(int i = 0; i < groupArrayList.size(); i++){
+			if(groupArrayList.get(i).getNumber().equals(g.getNumber())){
+				groupArrayList.set(i, g);
+			}
+			prop.setProperty(String.valueOf(i).toString(), groupArrayList.get(i).toString());
+			
+		}
+		//
+		try {
+			prop.storeToXML(new FileOutputStream("hey_bro_project_BROS\\src\\Model\\Data\\xml\\members.xml"), String.valueOf(new Date()).toString());
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 	
 }
