331d58a4682064660d3931946d3b1c52d750e904
user2 <user2@KH_C>
2017-11-28 19:19:02 +0900

    GroupList

--- hey_bro_project_BROS/src/View/GroupListForLeaders.java
+++ hey_bro_project_BROS/src/View/GroupListForLeaders.java
@@ -3,8 +3,14 @@ package View;
 import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.Image;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 import java.util.Calendar;
 import java.util.GregorianCalendar;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Map;
+import java.util.Properties;
 
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
@@ -16,8 +22,23 @@ import javax.swing.JTable;
 import javax.swing.table.DefaultTableModel;
 import javax.swing.table.JTableHeader;
 
-public class GroupListForLeaders  {
-	public void groupList(){
+import Model.vo.Group;
+import Model.vo.Member;
+import Model.vo.Schedule;
+import Model.vo.Session;
+
+public class GroupListForLeaders  extends JPanel{
+	
+	private JPanel mainPanel;
+	private JFrame superFrame;
+	private Member m = new Member();
+	private Group g = new Group();
+	private Schedule s = new Schedule();
+	private Session session = new Session();
+	Properties prop = new Properties();
+	
+	
+	public GroupListForLeaders(Session session){
 	
 		JFrame mf = new JFrame();		
 		mf.setBounds(300, 100, 800, 500);
@@ -30,19 +51,20 @@ public class GroupListForLeaders  {
 		panel.setBounds(0, 0, 800, 500);
 		
 		
-		JLabel lb1 = new JLabel("BROS");
+		JLabel lb1 = new JLabel(g.getgName());
 		lb1.setBounds(30, 10, 100, 40);
 		lb1.setFont(new Font(Font.DIALOG, Font.BOLD, 16));
 		
-		Image icon = new ImageIcon("hey_bro_project_BROS/src/View/img/images.png").getImage().getScaledInstance(40, 40, 0);
+		Image icon = new ImageIcon(m.getPictureUrl()).getImage().getScaledInstance(48, 45, 0);;
 		JLabel lb2 = new JLabel(new ImageIcon(icon));
 		lb2.setBounds(1, 28, 80, 80);
 		
-		JLabel lb3 = new JLabel("조성식님은 모임장입니다.");
+		//Profile 
+		JLabel lb3 = new JLabel(m.getUserId() + "은 모임장입니다.");								
 		lb3.setBounds(70, 20, 200, 100);
 		lb3.setFont(new Font(Font.DIALOG, Font.BOLD, 14));
 		
-		JLabel lb4 = new JLabel("BROS는 KH정보교육원의 1조 모임으로..");
+		JLabel lb4 = new JLabel(g.getContent());
 		lb4.setBounds(20, 60, 250, 110);
 		lb4.setFont(new Font(Font.DIALOG, Font.BOLD, 12));
 		
@@ -55,27 +77,12 @@ public class GroupListForLeaders  {
 		JButton btn3 = new JButton("모임 삭제");
 		btn3.setBounds(20, 350, 220, 50);
 		
-		Image icon2 = new ImageIcon("hey_bro_project_BROS/src/View/img/if_button-previous_855910.png").getImage().getScaledInstance(30, 30, 0);
-		JLabel prev = new JLabel(new ImageIcon(icon2));
-		prev.setBounds(430, 50,30,30 );
 		
-		JLabel year = new JLabel();
-		Calendar today = new GregorianCalendar();
-		String cyear = Integer.valueOf(today.get(Calendar.YEAR)).toString(); 
-		year.setText(cyear);
-		year.setBounds(512, 16, 200, 100);
-		year.setFont(new Font(Font.DIALOG, Font.BOLD, 20));
-		
-		
-		
-		Image icon3 = new ImageIcon("hey_bro_project_BROS/src/View/img/if_button-next_855870.png").getImage().getScaledInstance(30, 30, 0);
-		JLabel next = new JLabel(new ImageIcon(icon3));
-		next.setBounds(600, 50,30,30 );
-		
+			
 		 
 		JLabel currentMonth = new JLabel();
-	
-		String cmonth = Integer.valueOf(today.get(Calendar.MONTH)).toString() + "월"; 
+		Calendar today = new GregorianCalendar();
+		String cmonth = Integer.valueOf(today.get(Calendar.MONTH)+1).toString() + "월"; 
 		currentMonth.setText(cmonth);
 		currentMonth.setBounds(300, 75, 150, 100);
 		currentMonth.setFont(new Font(Font.DIALOG, Font.BOLD, 18));
@@ -83,10 +90,11 @@ public class GroupListForLeaders  {
 		//table1
 		
 		
-		 Object[][] data= {		            
-	  	      {"16","15:00" , "17:00", "프로젝트 기획 발표", "발표"},
-	  	      {"18","11:00" , "17:00", "보고서 제출", "보고서 작성"},		  	      
-	  	      };
+		Object [][] data = null;
+		for(int i =0; i<prop.size();i++){			
+			data[i] = new Object[] {s.getDate(), s.getStartHour(), s.getStartMin(), s.getEndHour(), s.getEndMin(), s.getsContent()};			
+		}
+		
 		 String[] columnNames = {"일", "시작시간", "종료시간", "일정명" , "일정내용" };		
 		 
 
@@ -114,43 +122,41 @@ public class GroupListForLeaders  {
 	      JScrollPane sp = new JScrollPane(table);
 	     
 		
-	      //table 2
-	      
-	      
-	      Object[][] data2= {		            
-		  	      {"16","15:00" , "17:00", "프로젝트 기획 발표", "발표"},
-		  	      {"18","11:00" , "17:00", "보고서 제출", "보고서 작성"},		  	      
-		  	      };
-			 String[] columnNames2 = {"일", "시작시간", "종료시간", "일정명" , "일정내용" };		
-			 
+		 JButton back = new JButton("뒤로 가기");
+		  back.setBounds(650,20,100,30);
+		  back.addMouseListener(new MouseListener(){
 
-		      DefaultTableModel dtm2 = new DefaultTableModel(	data2, columnNames2	  
-		    		/* new Object[][]  {		            
-									  	      {"16","15:00" , "17:00", "프로젝트 기획 발표", "발표"},
-									  	      {"18","11:00" , "17:00", "보고서 제출", "보고서 작성"},		  	      
-									  	      },		  	      
-			  	     new String[] {"일", "시작시간", "종료시간", "일정명" , "일정내용" }*/
-			  	      );
-		      
-		      JTable table2 = new JTable(dtm2);
-		      table2.setBounds(290, 270, 420, 30);
-		      table2.setAutoCreateRowSorter(true);
-		      table2.getTableHeader().setReorderingAllowed(false);
-		      
-		      JTableHeader th2 = table2.getTableHeader();
-		      th2.setBounds(290, 250, 420, 20);
-		      th2.setReorderingAllowed(false);
-		      th2.setResizingAllowed(false);
-		      
-		      table2.setFillsViewportHeight(true);
-		      table2.setPreferredScrollableViewportSize(new Dimension(500, 150));
-			
-		      JScrollPane sp2 = new JScrollPane( table2);
-	      
-		      
-		      JButton calendar = new JButton("Calendar");
-		      calendar.setBounds(650,20,100,30);
-		      
+				@Override
+				public void mouseClicked(MouseEvent e) {
+					// TODO Auto-generated method stub
+					
+				}
+
+				@Override
+				public void mousePressed(MouseEvent e) {
+					// TODO Auto-generated method stub
+					
+				}
+
+				@Override
+				public void mouseReleased(MouseEvent e) {
+				
+					
+				}
+
+				@Override
+				public void mouseEntered(MouseEvent e) {
+					// TODO Auto-generated method stub
+					
+				}
+
+				@Override
+				public void mouseExited(MouseEvent e) {
+					// TODO Auto-generated method stub
+					
+				}
+				 
+			 });
 	      
 	      
 		panel.add(lb1);
@@ -159,18 +165,12 @@ public class GroupListForLeaders  {
 		panel.add(lb4);
 		panel.add(btn);
 		panel.add(btn2);
-		panel.add(btn3);
-		panel.add(prev);
-		panel.add(next);
-		panel.add(year);
+		panel.add(btn3);	
 		panel.add(currentMonth);
 		panel.add(th);
 		panel.add(table);
-		panel.add(sp);
-		panel.add(th2);
-		panel.add(table2);
-		panel.add(sp2);
-		panel.add(calendar);
+		panel.add(sp);	
+		panel.add(back);
 		
 		mf.add(panel);
 	
@@ -178,13 +178,8 @@ public class GroupListForLeaders  {
 		mf.setVisible(true);
 		mf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
-		
-	}
+	}	
 	
 	
-	public static void main(String[] args){
-		GroupListForLeaders gl = new GroupListForLeaders();
-		gl.groupList();
-	}
 }
 
